ac1e0503a578f13a8534c20b57f5d04ea3329eff
Michael de Radobýl <mjohne@users.noreply.github.com>
2025-02-15 03:11:02 +0100

    Add files via upload

---
+++ Persyel/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Persyel
+{
+	[DebuggerDisplay(value: "{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private string GetDebuggerDisplay()
+		{
+			return ToString();
+		}
+	}
+}
